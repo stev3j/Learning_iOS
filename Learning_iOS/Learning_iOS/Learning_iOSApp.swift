57057ab5fb55f1e3177b03ef473bf94f7793dda6
stev3j <mygajeong@gmail.com>
2023-09-30 21:15:23 +0900

    🎉 :: Start Learning_iOS!!

---
+++ Learning_iOS/Learning_iOS/Learning_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_iOSApp.swift
+//  Learning_iOS
+//
+//  Created by 조승완 on 2023/09/30.
+//
+
+import SwiftUI
+
+@main
+struct Learning_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
